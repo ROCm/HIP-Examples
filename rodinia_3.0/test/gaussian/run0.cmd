74ef1411bb4e4453f354050b44471542049fd4f7
Maneesh Gupta <maneesh.gupta@amd.com>
2016-04-22 11:24:22 +0530

    Split gaussian sample's tests into individual tests

--- rodinia_3.0/test/gaussian/run0.cmd
+++ rodinia_3.0/test/gaussian/run0.cmd
@@ -1,6 +1,1 @@
 ./gaussian -f ../../data/gaussian/matrix3.txt
-./gaussian -f ../../data/gaussian/matrix4.txt
-./gaussian -f ../../data/gaussian/matrix16.txt
-./gaussian -f ../../data/gaussian/matrix208.txt
-./gaussian -f ../../data/gaussian/matrix1024.txt
- 
