5554fce77954289d9786e1da0af30f3f2d525367
Andres Rodriguez <andres.rodriguez@amd.com>
2016-05-31 18:40:49 -0400

    gpu-burn: gpuburn refactor/rewrite
    Re-organize gpu-burn into a more hip friendly format.
    
    Changes:
            - Thread based
            - Removed dependency on AGI
            - Compare kernel not yet supported
    
    Change-Id: I6530e8aa4e25d6b106ce4a257e90335d3605fbec

---
+++ gpu-burn/BurnKernel.cpp
@@ -0,0 +1,158 @@
+/*
+ * Public domain.  No warranty.
+ * Ville Timonen 2013
+ * edited by Timmy Liu for HIP API 01/2016
+ */
+
+#include <iostream>
+#include <thread>
+#include <hip_runtime.h>
+
+#include "common.h"
+#include "BurnKernel.h"
+
+// ---------------------------------------------------------------------------
+namespace gpuburn {
+
+BurnKernel::BurnKernel(int hipDevice)
+    : mHipDevice(hipDevice), mRunKernel(false),
+    mDeviceAdata(NULL), mDeviceBdata(NULL), mDeviceCdata(NULL)
+{
+}
+
+BurnKernel::~BurnKernel()
+{
+    if (mDeviceAdata)
+        hipFree(mDeviceAdata);
+
+    if (mDeviceBdata)
+        hipFree(mDeviceBdata);
+
+    if (mDeviceCdata)
+        hipFree(mDeviceCdata);
+
+    if (mBurnThread)
+        mBurnThread->join();
+}
+
+// ---------------------------------------------------------------------------
+
+extern "C" __global__ void hip_sgemm_kernel(hipLaunchParm lp, const int M,
+                                            const int N, const int K,
+                                            const float alpha,
+                                            float *A, const int lda, float *B,
+                                            const int ldb, const float beta,
+                                            float *C, const int ldc)
+{
+        //column major NN
+        size_t idx_x = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+        size_t idx_y = hipBlockIdx_y * hipBlockDim_y + hipThreadIdx_y;
+        size_t dim_x = hipGridDim_x * hipBlockDim_x;
+        size_t myIdx = idx_y * dim_x + idx_x;
+
+        float local_c = beta * C[myIdx];
+
+        for(int k = 0; k < K; k++) {
+          local_c += alpha * A[ idx_y + k * K] * B[ idx_x * K + k];
+        }
+
+        C[myIdx] = local_c;
+}
+
+// ---------------------------------------------------------------------------
+
+int BurnKernel::Init()
+{
+    int hipDevice = bindHipDevice();
+
+    std::string msg = "Init Burn Thread for device (" + std::to_string(hipDevice) + ")\n";
+    std::cout << msg;
+
+    srand(cRandSeed);
+    for (int i = 0; i < cMatrixSize; ++i) {
+        mHostAdata[i] = (rand() % 1000000)/100000.0;
+        mHostBdata[i] = (rand() % 1000000)/100000.0;
+    }
+
+    size_t freeMem = getAvailableMemory() * cUseMem;
+    size_t matrixSizeBytes = sizeof(float)*cMatrixSize;
+    mNumIterations = (freeMem - (matrixSizeBytes*2))/matrixSizeBytes;
+
+    checkError(hipMalloc((void**)&mDeviceAdata, matrixSizeBytes), "Alloc A");
+    checkError(hipMalloc((void**)&mDeviceBdata, matrixSizeBytes), "Alloc B");
+    checkError(hipMalloc((void**)&mDeviceCdata, matrixSizeBytes*mNumIterations), "Alloc C");
+
+    checkError(hipMemcpy(mDeviceAdata, mHostAdata, matrixSizeBytes, hipMemcpyHostToDevice), "A -> device");
+    checkError(hipMemcpy(mDeviceBdata, mHostBdata, matrixSizeBytes, hipMemcpyHostToDevice), "B -> device");
+    checkError(hipMemset(mDeviceCdata, 0, matrixSizeBytes*mNumIterations), "C memset");
+
+    return 0;
+}
+
+int BurnKernel::startBurn()
+{
+    mRunKernel = true;
+
+    mBurnThread = make_unique<std::thread>(&BurnKernel::threadMain, this);
+    return 0;
+}
+
+int BurnKernel::threadMain()
+{
+    int err = 0;
+    int hipDevice = bindHipDevice();
+    std::string msg = "Burn Thread using device (" + std::to_string(hipDevice) + ")\n";
+    std::cout << msg;
+
+    while (mRunKernel) {
+        err = runComputeKernel();
+    }
+
+    return err;
+}
+
+int BurnKernel::stopBurn()
+{
+    mRunKernel = false;
+    return 0;
+}
+
+int BurnKernel::bindHipDevice()
+{
+    int hipDevice = -1;
+    hipSetDevice(mHipDevice);
+    hipGetDevice(&hipDevice);
+    return hipDevice;
+}
+
+int BurnKernel::runComputeKernel()
+{
+    int err = 0;
+
+    for (int i = 0; mRunKernel && i < mNumIterations; ++i) {
+        hipLaunchKernel(
+            /* Launch params */
+            HIP_KERNEL_NAME(hip_sgemm_kernel),
+            dim3(cRowSize/cBlockSize, cRowSize/cBlockSize, 1),
+            dim3(cBlockSize,cBlockSize,1), 0, 0,
+            /* Kernel params */
+            cRowSize, cRowSize, cRowSize, cAlpha,
+            mDeviceAdata, cRowSize,
+            mDeviceBdata, cRowSize,
+            cBeta,
+            mDeviceCdata + i*cMatrixSize,
+            cRowSize);
+    }
+    hipDeviceSynchronize();
+
+    return err;
+}
+
+size_t BurnKernel::getAvailableMemory()
+{
+    size_t freeMem, totalMem;
+    checkError(hipMemGetInfo(&freeMem, &totalMem));
+    return freeMem;
+}
+
+}; //namespace gpuburn
---
+++ gpu-burn/gpuburn.cpp
@@ -0,0 +1,118 @@
+/*
+ * Public domain.  No warranty.
+ * Ville Timonen 2013
+ * edited by Timmy Liu for HIP API 01/2016
+ */
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <memory>
+#include <vector>
+#include <unistd.h>
+#include <hip_runtime.h>
+
+#include "common.h"
+#include "AmdGpuMonitor.h"
+#include "BurnKernel.h"
+
+// ---------------------------------------------------------------------------
+using namespace gpuburn;
+
+std::vector<std::unique_ptr<BurnKernel>> genBurnKernels()
+{
+    int deviceCount = 0;
+    std::vector<std::unique_ptr<BurnKernel>> kernels;
+
+    try {
+        checkError(hipGetDeviceCount(&deviceCount));
+    } catch (std::string e) {
+        std::cerr << "Error: couldn't find any HIP devices\n";
+    }
+
+    for (int i =0; i < deviceCount; ++i) {
+        try {
+            std::unique_ptr<BurnKernel> kernel(new BurnKernel(i));
+            kernel->Init();
+            kernels.push_back(std::move(kernel));
+        } catch (std::string e) {
+            std::cerr << e;
+            std::cerr << "Error: failed to initialize hip device " << i << "\n";
+        }
+    }
+
+    return kernels;
+}
+
+std::vector<std::unique_ptr<GpuMonitor>> genGpuMonitors()
+{
+    int deviceCount = 0;
+    std::vector<std::unique_ptr<GpuMonitor>> monitors;
+
+    for (int i = 0; true; i++) {
+            std::string hwmonName;
+            std::ifstream hwmon("/sys/class/hwmon/hwmon" + std::to_string(i) + "/name");
+
+            if (!hwmon.good())
+                break;
+
+            hwmon >> hwmonName;
+            if (hwmonName == "amdgpu") {
+                GpuMonitor* monitor = new AmdGpuMonitor(i, "/sys/class/hwmon/hwmon" + std::to_string(i));
+                std::unique_ptr<GpuMonitor> uniq_monitor(monitor);
+                monitors.push_back(std::move(uniq_monitor));
+            }
+    }
+
+    return monitors;
+}
+
+int doBurn(int burnSec) {
+    std::vector<std::unique_ptr<BurnKernel>> burnKernels = genBurnKernels();
+    std::vector<std::unique_ptr<GpuMonitor>> gpuMonitors = genGpuMonitors();
+
+    if (burnKernels.size() == 0)
+        return -ENOENT;
+
+    for (auto& kernel : burnKernels) {
+        kernel->startBurn();
+    }
+
+    for (; burnSec > 0; --burnSec) {
+        std::ostringstream msg;
+        msg << "Temps: ";
+        for (auto& monitor : gpuMonitors) {
+            msg << "[GPU" << monitor->getId() << ":" << monitor->getTemperature() << "C] ";
+        }
+        msg << burnSec << "s\n";
+        std::cout << msg.str();
+        sleep(1);
+    }
+
+    for (auto& kernel : burnKernels) {
+        kernel->stopBurn();
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int opt;
+    int burnSec = 10;
+
+    while ((opt = getopt (argc, argv, "ht:")) != -1)
+        switch (opt)
+        {
+            case 't':
+                burnSec = atoi(optarg);
+                break;
+            case 'h':
+            default:
+                std::cerr << "Usage: " << argv[0] << " [-t sec]\n";
+                return -EINVAL;
+        }
+
+    return doBurn(burnSec);
+}
+
+
+// ---------------------------------------------------------------------------
